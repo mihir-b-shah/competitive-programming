67482e7785cfcc169fdac49c85b1a230a143656c
mihir-b-shah <46288414+mihir-b-shah@users.noreply.github.com>
2019-11-04 14:51:31 -0600

    io still not working

--- usaco_cpp/chapter4/s3_bignums/main.cpp
+++ usaco_cpp/chapter4/s3_bignums/main.cpp
@@ -1,6 +1,31 @@
-#include "race3.cpp"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
 
 int main() {
-    solve();
+    ifstream fin;
+    ofstream fout;
+
+    fin.open("race3.in");
+    fout.open("race3.out");
+
+    int buf;
+    bool outer = 1;
+    bool inner = 1;
+
+    while(outer) {
+        while(inner) {
+            fin >> buf;
+
+            switch(buf) {
+                case -2: cout << buf << ' '; inner = 0; break;
+                case -1: cout << buf << ' '; outer = 0; break;
+                default: cout << buf << ' ';
+            }
+        }
+        inner = true;
+    }
+
     return 0;
 }
--- usaco_cpp/chapter4/s3_bignums/race3.cpp
+++ usaco_cpp/chapter4/s3_bignums/race3.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <vector>
 #include <queue>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -26,15 +28,38 @@ typedef struct node {
 };
 
 typedef vector<int> vi;
+vector<vi> vii;
 
-int solve() {
+bool cycle(int start) {
+    queue<int> q;
+    q.push(start);
+    long mem = 1L;
+    int n;
+    int ref;
+
+    while(!q.empty()) {
+        n = q.front();
+        vi next = vii[n];
+        for(int i = 0; i<next.size(); ++i) {
+            if((ref = next[i]) == start) {
+                return 1;
+            }
+            if((mem & 1L << ref) == 0) {
+                q.push(ref);
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main() {
     ifstream fin;
     fin.open("race3.in");
     
     ofstream fout;
     fout.open("race3.out");
 
-    vector<vi> vii;
     int buf;
     bool outer = 1;
     bool inner = 1;
@@ -45,37 +70,75 @@ int solve() {
             fin >> buf;
 
             switch(buf) {
-                case -2: inner = true; break;
-                case -1: outer = true; break;
+                case -2: inner = false; break;
+                case -1: outer = false; break;
                 default: vect.push_back(buf);
             }
         }
         vii.push_back(vect);
     }
 
+    fin.close();
     vii.pop_back();
-    
-    queue<node*> q;
-    q.push(&node(0));
+
+    /*
+
+    queue<node> q;
+    q.push(node(0));
     int d = vii.size()-1;
     long agr = (1 << vii.size()) - 1;
 
-    while(!q.empty) {
-        node* n = q.back();
+    while(!q.empty()) {
+        node n = q.front();
         q.pop();
-        if(n->id=d) {
-            agr &= n->mask;
+        if(n.id=d) {
+            agr &= n.mask;
         }
 
-        vi* next = &vii[n->id];
-        const int lim = next->size();
+        const int lim = vii[n.id].size();
         for(int i = 0; i<lim; ++i) {
-            if(!n->det(next->at(i))) {
-                q.push(&node(n->mask,next->at(i)));
+            if(!n.det(vii[n.id].at(i))) {
+                q.push(node(n.mask,vii[n.id][i]));
             }
         }
     }
 
+    string split;
+    split.reserve(100);
+    string out1; 
+    out1.reserve(100);
+
+    for(int i = 1; i < vii.size(); ++i) {
+        if((agr & 1 << i) != 0) {
+            out1 += i;
+            out1 += ' ';
+        }
+    }
+
+    int loc_ref;
+    for(int i = 0; i<out1.size(); i+=2) {
+        if(!cycle(loc_ref = out1.at(i)-'0')) {
+            split += loc_ref;
+            split += ' ';
+        }
+    }
+
+    if(out1.size() > 0) {
+        out1.pop_back();
+        fout << (1 + out1.size() >> 1) << ' ' << out1 << endl;
+    } else {
+        fout << 0 << endl;
+    }
+
+    if(split.size() > 0) {
+        split.pop_back();
+        fout << (1 + split.size() >> 1) << ' ' << split << endl;
+    } else {
+        fout << 0 << endl;
+    }
+
+    */
+
     fout.close();
-    fin.close();
+    return 0;
 }
