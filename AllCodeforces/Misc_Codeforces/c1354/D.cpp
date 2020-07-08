d4a40016af586eda76d1fd42613a271502aaee74
Mihir Shah <mihirshah.11204@gmail.com>
2020-07-08 17:01:14 -0500

    YEEET 1354D worked!

--- AllCodeforces/Misc_Codeforces/c1354/D.cpp
+++ AllCodeforces/Misc_Codeforces/c1354/D.cpp
@@ -1,9 +1,11 @@
 
+#pragma GCC optimize("Ofast")
+
 #include <iostream>
 
 class Tree {
     private:
-        static constexpr int nextTree(int n){
+        int nextTree(int n){
             n = n | (n >> 1);
             n = n | (n >> 2);
             n = n | (n >> 4);
@@ -14,54 +16,100 @@ class Tree {
         struct Info {
             int num;
             int ct;
-            int size;
-            
+            int left;
+
             Info(){}
         };
         Info* tree;
-        int N;
         int size;
     public:
         Tree(int N){
-            this->N = N;
             size = nextTree(N);
             tree = new Info[size];
             for(int i = 0; i<size; ++i){
                 tree[i].num = i+1;
                 tree[i].ct = 0;
-                tree[i].size = 0;
+                tree[i].left = 0;
             }
         }
         ~Tree(){
             delete tree;
         }
         void insert(int v){
-            int l = 1;
-            int u = size;
-            int med; 
-            int jmp = (l+u)>>1;
-            
-            while(l <= u){
-                med = (l+u)>>1;
-                jmp >>= 1;
-                
-                if(v > tree[med].num){
-                    l = med+1;
-                } else if(v < tree[med].num){
-                    u = med-1;
-                } else {
-                    // equal
-                }
-            }
+			int start = (size+1) >> 1;
+			int jmp = (size+1) >> 2;
+			
+			while(jmp >= 0){
+				if(v > start){
+					start += jmp;
+				} else if(v < start){
+					++tree[start-1].left;
+					start -= jmp;
+				} else {
+					++tree[start-1].ct;	
+					break;
+				}
+				jmp >>= 1;
+			}	
         }
-        void erase(int k){
+        void erase(int k) {
+			k -= 1;
+			int start = (size+1) >> 1;
+			int jmp = (size+1) >> 2;
+			
+			while(jmp >= 0){
+				if(k >= tree[start-1].left+tree[start-1].ct){
+					k -= tree[start-1].left + tree[start-1].ct;
+					start += jmp;	
+				} else if(k < tree[start-1].left){
+					--tree[start-1].left;
+					start -= jmp;
+				} else {
+					--tree[start-1].ct;
+					break;
+				}
+				jmp >>= 1;
+			}	
         }
-}
+		void print(std::ostream& out){
+			for(int i = 0; i<size; ++i){
+				out << '(' << tree[i].num<< ' ' << tree[i].ct<< ' ' << tree[i].left << ')' << ' ';   
+			}
+			out << '\n';
+		}
+		int find(){
+			for(int i = 0; i<size; ++i){
+				if(tree[i].ct > 0){
+					return tree[i].num;
+				}
+			}	
+			return 0; 
+		}
+};
 
 int main(){
     int N,Q;
-    std::cin >> N >> Q;
+	std::ios_base::sync_with_stdio(0); 
+	std::cin.tie(0);	
+
+	std::cin >> N >> Q;
     
-    Tree tree(N);
-    
+    Tree tree(N);   
+    int v;
+	for(int i = 0; i<N; ++i){
+		std::cin >> v;
+		tree.insert(v);
+	}
+	for(int i = 0; i<Q; ++i){
+		std::cin >> v;
+		if(v > 0){
+			tree.insert(v);
+		} else {
+			tree.erase(-v);
+		}
+	}
+
+	std::cout << tree.find() << '\n';
+
+	return 0;
 }
