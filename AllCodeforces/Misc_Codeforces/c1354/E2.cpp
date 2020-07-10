eaada1b21a8184fb3b034424dfc111edc5c171f5
Mihir Shah <mihirshah.11204@gmail.com>
2020-07-10 00:20:12 -0500

    tle on case 5

--- AllCodeforces/Misc_Codeforces/c1354/E2.cpp
+++ AllCodeforces/Misc_Codeforces/c1354/E2.cpp
@@ -4,11 +4,12 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <unordered_map>
 
 using namespace std;
 
 class Matrix {
-	bool* __restrict matrix;
+	bool* matrix;
 	int V;
 	int D;
 
@@ -22,14 +23,14 @@ class Matrix {
 			delete matrix;
 		}
 		bool get(int i, int j) const {
-			return matrix[V*i+j];
+			return matrix[D*i+j];
 		}
 		void set(int i, int j, bool v){
-			matrix[V*i+j] = v;
+			matrix[D*i+j] = v;
 		}
 		void print(std::ostream& out){
-			for(int i = 0; i<D; ++i){
-				for(int j = 0; j<V; ++j){
+			for(int i = 0; i<V; ++i){
+				for(int j = 0; j<D; ++j){
 					out << get(i,j) << ' ';
 				}
 				out << '\n';
@@ -40,7 +41,7 @@ class Matrix {
 int V,E;
 Matrix* matrix;
 
-enum Color {WHITE, RED, BLUE};
+enum Color {WHITE, RED, BLUE, GREEN};
 Color colors[5000] = {WHITE};
 
 int colorCts[3] = {0};
@@ -61,8 +62,12 @@ static inline Color next(Color c){
 	}
 }
 
+vector<vector<int>> groups;
+int dfsCtr = 0;
+
 bool dfs(int src, Color color){
 	colors[src] = color;
+	groups[dfsCtr].push_back(src);
 	++colorCts[color];
 	bool chk;
 	bool aggr = true;
@@ -95,6 +100,10 @@ struct Pair {
 	}
 };
 
+long long myhash(long long tgt, int v){
+	return (tgt << 32) + v;
+}
+
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -116,28 +125,76 @@ int main(){
 
 	int next;
 	vector<Pair> options;
+	groups.resize(V);
 
 	while((next = scan()) < V){
 		colorCts[1] = 0; colorCts[2] = 0;
-		dfs(next, RED);
-
+		bool res = dfs(next, RED);
+		if(!res){
+			cout << "NO" << endl;
+			return 0;
+		}
+		
+		++dfsCtr;
 		options.push_back(Pair(colorCts[RED], colorCts[BLUE]));
-		cout << "Hi im here.\n";
 	}
 
-	Matrix dp(V+1, options.size()+1);
+	Matrix dp(options.size()+1, V+1);
 	dp.set(0, 0, true);
+	unordered_map<long long, long long> lookup;
 
 	bool pred1,pred2;
 	for(int i = 1; i<options.size()+1; ++i){
 		for(int j = 1; j<V+1; ++j){
-			pred1 = j >= options[i].one ? dp.get(j-options[i].one, i-1) : false;
-			pred2 = j >= options[i].two ? dp.get(j-options[i].two, i-1) : false;
-			dp.set(j, i, dp.get(j,i) || pred1 || pred2);
+			pred1 = j >= options[i-1].one ? dp.get(i-1, j-options[i-1].one) : false;
+			pred2 = j >= options[i-1].two ? dp.get(i-1, j-options[i-1].two) : false;
+			if(pred1){
+				lookup[myhash(j,i)] = myhash(j-options[i-1].one, i-1); 
+			} else if(pred2){
+				lookup[myhash(j,i)] = myhash(j-options[i-1].two, i-1);
+			}
+			dp.set(i, j, pred1 || pred2);
 		}
 	}
-	
-	dp.print(cout);
-	cout << (dp.get(options.size(), V) ? "YES" : "NO") << '\n';
+
+	cout << (dp.get(options.size(), tgtRed) ? "YES" : "NO") << endl;
+	if(dp.get(options.size(), tgtRed)){
+		// time to backtrace.
+		long long hash = myhash(tgtRed, options.size());
+		int v = tgtRed; int idx = options.size();
+		int newV, newIdx;
+		int backCtr = 0;
+		while(lookup.find(hash) != lookup.end()){
+			long long next = lookup[hash];
+			newIdx = next & 0xffffffffLL;
+			newV = next >> 32;
+			hash = myhash(newV, newIdx);
+			int option = v-newV;
+			if(options[idx-1].one != option){
+				vector<int>& myvect = groups[groups.size()-1-backCtr];
+				for(int e: myvect){
+					if(colors[e] == RED){
+						colors[e] = BLUE;
+					} else {
+						colors[e] = RED;
+					}
+				}	
+			}
+			v = newV;
+			idx = newIdx;
+			++backCtr;
+		}
+
+		// allocate evenly between red/green.
+		int redCtr = 0;
+		for(int i = 0; i<V; ++i){
+			if(redCtr < tgtRed/2 && colors[i] == RED){
+				++redCtr;
+			} else if(colors[i] == RED){
+				colors[i] = GREEN;
+			}
+			cout << colors[i];
+		} cout << '\n';
+	}
 	return 0;
 }
